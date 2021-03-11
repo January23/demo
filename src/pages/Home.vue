6d5840d63769ffc34ff0f413cbc5444eb17e1db3
jiezhao_3 <jiezhao_3@tujia.com>
2021-03-11 21:57:57 +0800

    update

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{x.name}}</div>
+</template>
+
+<script lang="ts" setup>
+import { User} from '../entity/type';
+
+const x:User = {name:'xxxx', age:1}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
